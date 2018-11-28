d82da3315dcccafed17ab86438d5dc23c3a72f16
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-11-28 09:42:40 -1000

    header added

---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'app-header',
+  //template: `<button>Click Me</button>`
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent{
+  
+}
--- src/app/pages/app.module.ts
+++ src/app/pages/app.module.ts
@@ -2,17 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { HeaderComponent } from './component/header/header.component';
-
+import { AppComponent } from '../app.component';
+import { TestComponent } from '../test/test.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    HeaderComponent
 
   ],
   imports: [
